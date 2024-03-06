0de884272c6a9bf38d28f2e83160d418c2405c0e
ravidasari0789 <ravidasari0789@gmail.com>
2024-03-06 09:36:49 +0530

    rounting app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'rountingApp';
+
+  msg:string="Rounting Example Application..!!!!";
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from "./home/home.component";
+import { ServiceComponent } from './service/service.component';
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'service', component:ServiceComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'', redirectTo:"/home", pathMatch:'full'}
+];
